cb365162baa4850759d4cc0e853a2d7275e29a5f
daniellefag <danielle.fag@gmail.com>
2021-02-23 19:46:39 -0300

    Metodo para incluir categoria - Post

--- src/main/java/com/cursoudemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursoudemy/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,37 @@
 package com.cursoudemy.cursomc.resources;
 
-
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursoudemy.cursomc.domain.Categoria;
 import com.cursoudemy.cursomc.services.CategoriaService;
 
-
-
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	 public ResponseEntity<?> find(@PathVariable Integer id) {
-	   Categoria obj = service.buscar(id);
-	   return ResponseEntity.ok().body(obj);
-	 }
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria categoria = service.buscar(id);
+		return ResponseEntity.ok().body(categoria);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
