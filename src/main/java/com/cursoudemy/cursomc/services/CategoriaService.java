2d58c359d64c305f5610596cf04b7f3c57079aec
daniellefag <danielle.fag@gmail.com>
2021-02-24 19:57:56 -0300

    método para realizar update na categoria

--- src/main/java/com/cursoudemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/cursoudemy/cursomc/services/CategoriaService.java
@@ -20,14 +20,18 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 	
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repository.findById(id);
-		
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	public Categoria find(Integer id) {
+		Optional<Categoria> categoria = repository.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert (Categoria categoria) {
 		categoria.setId(null);
+		return repository.save(categoria);
+	}
+	
+	public Categoria update (Categoria categoria) {
+		find(categoria.getId());
 		return repository.save(categoria);
 	}
 }
